5757be8012eb70bc73cc8079b9b24a26a84d11e3
danilsocol <danilsocol-2002@mail.ru>
2021-02-19 22:40:00 +0500

    Logic and Console

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ОПЗ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = FileWork.ReadFile();
+
+            int minRange = Convert.ToInt32(text[0]);
+            int maxRange = Convert.ToInt32(text[1]);
+            string example = text[2];
+
+            for (int i = minRange; i <= maxRange; i++)
+            {
+                OPZ.splitExample(example, i);
+            }
+        }
+    }
+}
